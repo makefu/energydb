91f54b10e4678c82240d85123382b0a33e8da130
makefu <github@syntax-fehler.de>
2012-04-22 20:30:34 +0200

    update skript to require less user interaction

--- utils/add_image.sh
+++ utils/add_image.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 IPATH='/home/makefu/repos/energydb/drinks/img/'
-if [ -z "$1" ] || [ -z "$2" ];then
-  echo "usage add_drink IMAGE DRINK_KEY"
+if [ -z "$1" ];then
+  echo "usage add_drink IMAGE"
   exit -1
 fi
+img=${1%.*}
 #TODO fix the absolute path stuff
-convert -resize x600 "$1" $IPATH"$2".jpg
-convert -resize x100 "$1" $IPATH"thumb_"$2.jpg
+convert -resize x600 "$1" $IPATH"$img".jpg
+convert -resize x100 "$1" $IPATH"thumb_"$img.jpg
